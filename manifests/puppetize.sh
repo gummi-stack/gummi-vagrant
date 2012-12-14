16402a3498212feb9cb19feeb7a107a2746a863a
root <root@node3.lxc.nag.ccl>
2012-12-14 17:10:49 +0100

    c

--- manifests/puppetize.sh
+++ manifests/puppetize.sh
@@ -1,2 +1,2 @@
 puppet apply --templatedir /root/gummi-vagrant/manifests/ -v -d /root/gummi-vagrant/manifests/default.pp
-rsync -av --delete --info=progress2 root@10.1.69.105:/var/lib/lxc/child /var/lib/lxc/
+rsync -av --delete  root@10.1.69.105:/var/lib/lxc/child /var/lib/lxc/
