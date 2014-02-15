b91e3b9698eb8b286bcc8de0b791240aea18987b
Martin Cizek <martin.cizek@socialbakers.com>
2014-02-15 10:58:58 +0100

    Node wision

--- manifests/default.pp
+++ manifests/default.pp
@@ -100,5 +100,29 @@ node "lestr" {
 }
 
 
+node "wision" {
+  exec { 'apt-get update':
+	 command => '/usr/bin/apt-get update'
+  }
+
+  include essentials
+
+  $node_version = "v0.10.25"  ## change also in supervisor erb (forced)
+
+  class { 'nvm':
+	 node_version => $node_version,
+  }
+
+  class { "toadwart":
+	  name => 'node3.nag.ccl',
+	  port => '80',
+	  wan => 'eth0',
+	  wan_ip => $ipaddress_eth0,
+	  lxc_iface => 'lxcbr0',
+	  lxc_address => '192.168.73.1/24',
+	}
+
+}
+
 
 
